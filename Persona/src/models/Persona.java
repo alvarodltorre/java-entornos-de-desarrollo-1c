50aa173d5bf7362db6fcb46627a140fa1a60b6ec
PROTH3X <edualvaro138@gmail.com>
2024-03-14 16:07:17 +0100

    proyecto personas creado

---
+++ Persona/src/models/Persona.java
@@ -0,0 +1,121 @@
+package models;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellidos;
+
+    /**
+     * 
+     */
+    private int edad;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellidos 
+     * @param edad
+     */
+    public Persona(String dni, String nombre, String apellidos, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * Modifica el DNI de la persona.
+     * @param dni
+     */
+    public void setDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve el DNI de la persona.
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica el nombre de la persona.
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve el nombre de la persona.
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica los apellidos de la persona.
+     * @param apellidos
+     */
+    public void setApellidos(String apellidos) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve los apellidos de la persona.
+     * @return
+     */
+    public String getApellidos() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica la edad de la persona.
+     * @param edad
+     */
+    public void setEdad(int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve la edad de la persona.
+     * @return
+     */
+    public int getEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * 
+     */
+    public void Operation6() {
+        // TODO implement here
+    }
+
+}
