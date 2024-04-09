770a72a6a9ad841f077fcc66237c421d1d47b9f0
PROTH3X <edualvaro138@gmail.com>
2024-04-09 17:25:45 +0200

    Proyecto Algoritmos creado

---
+++ Algoritmos/src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import util.Algoritmos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sumatorio = Algoritmos.sumatorio(3);
+	}
+
+}
